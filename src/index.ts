ea9d64eabaacca99be3ca999771a8b5dc71fede8
MacaroniX-B <76429751+MacaroniX-B@users.noreply.github.com>
2023-05-11 16:20:10 +0200

    Added some words

--- src/index.ts
+++ src/index.ts
@@ -1,52 +1,56 @@
 import {parser} from "./syntax.grammar"
 import {
-  LRLanguage,
-  LanguageSupport,
-  indentNodeProp,
-  foldNodeProp,
-  foldInside,
-  delimitedIndent,
-  HighlightStyle,
-  syntaxHighlighting
+    LRLanguage,
+    LanguageSupport,
+    indentNodeProp,
+    foldNodeProp,
+    foldInside,
+    delimitedIndent,
+    HighlightStyle,
+    syntaxHighlighting
 } from "@codemirror/language"
 import {Tag, styleTags, tags as t} from "@lezer/highlight"
 
 let customTags = {
-  vernacular: Tag.define()
+    vernacular: Tag.define(),
+    gallina: Tag.define()
 }
 
 export let highlight = HighlightStyle.define([
-  { tag: customTags.vernacular, color:"#39FF14" }
+  { tag: customTags.gallina, color:"#6637dd" },
+  { tag: customTags.vernacular, color: "#7872d0"}
 ])
 
 export const coqLanguage = LRLanguage.define({
-  parser: parser.configure({
-    props: [
-      indentNodeProp.add({
-        Application: delimitedIndent({closing: ")", align: false})
-      }),
-      foldNodeProp.add({
-        Application: foldInside
-      }),
-      styleTags({
-        Identifier: t.variableName,
-        Boolean: t.bool,
-        String: t.string,
-        LineComment: t.lineComment,
-        "( )": t.paren,
-        Vernacular: customTags.vernacular
-      })
-    ]
-  }),
-  languageData: {
-    commentTokens: {line: ";"}
-  }
+    parser: parser.configure({
+        props: [
+        indentNodeProp.add({
+            Application: delimitedIndent({closing: ")", align: false})
+        }),
+        foldNodeProp.add({
+            Application: foldInside
+        }),
+        styleTags({
+            Identifier: t.variableName,
+            Boolean: t.bool,
+            String: t.string,
+            LineComment: t.lineComment,
+            "( )": t.paren,
+            // extra words
+            Vernacular: customTags.vernacular,
+            gallina: customTags.gallina
+        })
+        ]
+    }),
+    languageData: {
+        commentTokens: {line: ";"}
+    }
 })
 
 export function coq() {
-  return new LanguageSupport(coqLanguage)
+    return new LanguageSupport(coqLanguage)
 }
 
 export function coqSyntaxHighlighting() {
-  return syntaxHighlighting(highlight);
+    return syntaxHighlighting(highlight);
 }
